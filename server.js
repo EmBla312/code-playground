25f85faf61cdebbb059b41db11c1eeafd74a837e
EmBla312 <43798516+EmBla312@users.noreply.github.com>
2020-02-16 13:46:17 -0600

    heroku setup

--- server.js
+++ server.js
@@ -23,4 +23,5 @@ app.set('port', process.env.PORT || 5000);
 const server = app.listen(app.get('port'), () => {
     console.log('Express running -> PORT ${server.address().port}');
 });
+app.use(express.static(path.join(__dirname)));
 
